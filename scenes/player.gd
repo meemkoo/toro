2098e2a685567b9e60ac14914167fb8790734782
Seabass1609 <87626044+Seabass1609@users.noreply.github.com>
2025-01-25 17:48:01 -0600

    Fixed all know movement bugs, added fov changes (values may need to be tweaked), headbobbing, and enemy death along with a reward for killing enemies.

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,84 +1,88 @@
-class_name Player extends CharacterBody3D
+extends CharacterBody3D
 
-@export_category("Player")
-@export_range(1, 35, 1) var speed: float = 10 # m/s
-@export_range(10, 400, 1) var acceleration: float = 100 # m/s^2
+var speed
+var WALK_SPEED = 5.0
+const SENSITIVITY = 0.003
+var falling = false
+var runmaxxing = 0
+var walk_vel = Vector3.ZERO
+var grav_vel = Vector3.ZERO
 
-@export_range(0.1, 3.0, 0.1) var jump_height: float = 1 # m
-@export_range(0.1, 3.0, 0.1, "or_greater") var camera_sens: float = 1
+#bob vars
+const BOB_FREQ = 2.0
+const BOB_AMP = 0.08
+var t_bob = 0.0
 
-var runmaxxing = 0;
-var point = Vector2.ZERO;
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = 9.8
 
-var jumping: bool = false
-var mouse_captured: bool = false
+#fov vars
+const BASE_FOV = 80.0
+const FOV_CHANGE = 1.5
 
-var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+@onready var hitbox = $Head/Camera3D/Area3D
 
-var move_dir: Vector2 # Input direction for movement
-var look_dir: Vector2 # Input direction for look/aim
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-var walk_vel: Vector3 # Walking velocity 
-var grav_vel: Vector3 # Gravity velocity 
-var jump_vel: Vector3 # Jumping velocity
 
-@onready var camera: Camera3D = $Camera3D
+func _unhandled_input(event):
+	
+	if WALK_SPEED >= 10:
+		WALK_SPEED = 10
+	
+	
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(80))
+		
+	if Input.is_action_pressed("w_pres"): runmaxxing = 1
+	
+	for body in hitbox.get_overlapping_bodies():
+				#print (body)
+				pass
+				if body.is_in_group("Enemy"):
+					pass
+					body.queue_free()
+					WALK_SPEED = WALK_SPEED * 1.1
+				if body.is_in_group("Wall"):
+					pass
+					WALK_SPEED = 5
 
-func _ready() -> void:
-    capture_mouse()
-    
-func _process(delta: float) -> void:
-    pass
+func _physics_process(delta):
 
-var mouse_sens = 0.3
-var camera_anglev=0
+# Add the gravity.
+	grav_vel = Vector3.ZERO if is_on_floor() else grav_vel.move_toward(Vector3(0, velocity.y - gravity, 0), gravity * delta)
 
-func _unhandled_input(event: InputEvent) -> void:
-    if event is InputEventMouseMotion:
-        camera.rotate_y(deg_to_rad(-event.relative.x*mouse_sens))
-        var changev=-event.relative.y*mouse_sens
-        if camera_anglev+changev>-50 and camera_anglev+changev<50:
-            camera_anglev+=changev
-            # camera.rotate_x(deg_to_rad(changev))
-        #look_dir = event.relative * 0.0001
-        #print(event.relative)
-        # if mouse_captured: _rotate_camera()
-    #if Input.is_action_just_pressed("jump"): jumping = true
-    #if Input.is_action_just_pressed("exit"): get_tree().quit()
-    if Input.is_action_pressed("w_pres"): runmaxxing = 1
+		
+	var move_dir = Vector2(0,1)# runmaxxing * -camera.transform.basis.z.normalized() # Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
+	var _forward: Vector3 = -(camera.global_transform.basis * Vector3(move_dir.x, 0, move_dir.y))
+	var walk_dir: Vector3 = Vector3(_forward.x, 0, _forward.z).normalized()
+	walk_vel = walk_vel.move_toward(walk_dir * WALK_SPEED * move_dir.length() * runmaxxing, 100 * delta)
+	velocity = walk_vel + grav_vel
+	move_and_slide()
 
-func _physics_process(delta: float) -> void:
-    if mouse_captured: _handle_joypad_camera_rotation(delta)
-    velocity = _walk(delta) + _gravity(delta)
-    move_and_slide()
+# head bob
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
+	
+	#FOV
+	var velocity_clamped = clamp(velocity.length(), 0.5, WALK_SPEED * 2)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 12)
+	
+	#print(camera.fov)
+	
+	print(WALK_SPEED)
 
-func capture_mouse() -> void:
-    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-    mouse_captured = true
 
-func release_mouse() -> void:
-    Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-    mouse_captured = false
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP
+	pos.x = cos(time *BOB_FREQ / 2) *BOB_AMP
+	return pos
 
-func _rotate_camera(sens_mod: float = 1.0) -> void:
-    camera.rotation.y -= look_dir.x * camera_sens * sens_mod
-    camera.rotation.x = clamp(camera.rotation.x - look_dir.y * camera_sens * sens_mod, -1.5, 1.5)
-    pass
-    
-func _handle_joypad_camera_rotation(delta: float, sens_mod: float = 1.0) -> void:
-    var joypad_dir: Vector2 = Vector2(0,0)#Input.get_vector("look_left","look_right","look_up","look_down")
-    if joypad_dir.length() > 0:
-        look_dir += joypad_dir * delta
-        _rotate_camera(sens_mod)
-        look_dir = Vector2.ZERO
-
-func _walk(delta: float) -> Vector3:
-    move_dir = Vector2(0,1)# runmaxxing * -camera.transform.basis.z.normalized() # Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
-    var _forward: Vector3 = -(camera.global_transform.basis * Vector3(move_dir.x, 0, move_dir.y))
-    var walk_dir: Vector3 = Vector3(_forward.x, 0, _forward.z).normalized()
-    walk_vel = walk_vel.move_toward(walk_dir * speed * move_dir.length() * runmaxxing, acceleration * delta)
-    return walk_vel
-
-func _gravity(delta: float) -> Vector3:
-    grav_vel = Vector3.ZERO if is_on_floor() else grav_vel.move_toward(Vector3(0, velocity.y - gravity, 0), gravity * delta)
-    return grav_vel
+	
